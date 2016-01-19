d3854b18f823dc3bbd7b4e6b9e66cc69be9f3142
Aaron <germuth@unbc.ca>
2016-01-18 16:28:45 -0800

    got motors working, added mouse back

--- js/Box2D/tests/dominos.js
+++ js/Box2D/tests/dominos.js
@@ -36,13 +36,19 @@ embox2dTest_dominos.prototype.setup = function() {
 
         var fd = new b2FixtureDef();
         fd.set_shape(shape);
-        fd.set_density(20.0);
-        fd.set_friction(0.2);
-        fd.get_filter().set_categoryBits(0x0001);
-        fd.get_filter().set_maskBits(0xFFFF & ~0x0002);
+        // fd.set_density(20.0);
+        fd.set_density(2.0);
+        // fd.set_friction(0.2);
+        // fd.get_filter().set_categoryBits(0x0001);
+        // fd.get_filter().set_maskBits(0xFFFF & ~0x0002);
 
         var jd = new b2RevoluteJointDef();
         jd.set_collideConnected(false);
+        /*
+        jd.set_enableMotor(true);
+        jd.set_maxMotorTorque(20.0);
+        jd.set_motorSpeed(5.0);
+        */
 
         var N = 10;
         var y = 15.0;
@@ -50,26 +56,27 @@ embox2dTest_dominos.prototype.setup = function() {
         //ropeDef.get_localAnchorA().Set(0.0, y);
 
         this.wormBody = new Array();
+        this.wormJoints = new Array();
 
         var prevBody;
-        for (var i = 0; i < N; ++i)
+        var wormJoint;
+        for (var i = 0; i < 2; ++i)
         {
             var bd = new b2BodyDef();
             bd.set_type(b2_dynamicBody);
             bd.set_position(new b2Vec2(0.5 + 1.0 * i, y));
-
             var body = world.CreateBody(bd);
-
             body.CreateFixture(fd);
 
             if(i != 0){
               var anchor = new b2Vec2(i, y);
               jd.Initialize(prevBody, body, anchor);
-              world.CreateJoint(jd);
+              wormJoint = world.CreateJoint(jd);
             }
 
             prevBody = body;
             this.wormBody.push(prevBody);//complexity?
+            this.wormJoints.push(wormJoint);
         }
 
         //ropeDef.set_localAnchorB(new b2Vec2(0,0));
@@ -104,112 +111,5 @@ embox2dTest_dominos.prototype.setup = function() {
         jd.Initialize(ground, seesawBody, anchor);
         jd.set_collideConnected(true);
         world.CreateJoint(jd);
-    }
-
-    //pendulum
-    {
-        var shape = new b2PolygonShape();
-        shape.SetAsBox(0.25, 0.25);
-
-        var bd = new b2BodyDef();
-        bd.set_type(b2_dynamicBody);
-        bd.set_position(new b2Vec2(-10.0, 15.0));
-        var b4 = world.CreateBody(bd);
-        b4.CreateFixture(shape, 10.0);
-
-        anchor.Set(-7.0, 15.0);
-        jd.Initialize(ground, b4, anchor);
-        world.CreateJoint(jd);
-    }
-
-    //cradle holding balls
-    var cradleBody;
-    {
-        var bd = new b2BodyDef();
-        bd.set_type(b2_dynamicBody);
-        bd.set_position(new b2Vec2(6.5, 3.0));
-        cradleBody = world.CreateBody(bd);
-
-        var shape = new b2PolygonShape();
-        var fd = new b2FixtureDef();
-
-        fd.set_shape(shape);
-        fd.set_density(10.0);
-        fd.set_friction(0.1);
-
-        shape.SetAsBox(1.0, 0.1, new b2Vec2(0.0, -0.9), 0.0);
-        cradleBody.CreateFixture(fd);
-
-        shape.SetAsBox(0.1, 1.0, new b2Vec2(-0.9, 0.0), 0.0);
-        cradleBody.CreateFixture(fd);
-
-        shape.SetAsBox(0.1, 1.0, new b2Vec2(0.9, 0.0), 0.0);
-        cradleBody.CreateFixture(fd);
-
-        anchor.Set(6.0, 2.0);
-        jd.Initialize(ground, cradleBody, anchor);
-        world.CreateJoint(jd);
-    }
-
-    //lid on top of cradle
-    var lidBody;
-    {
-        var shape = new b2PolygonShape();
-        shape.SetAsBox(1.0, 0.1);
-
-        var bd = new b2BodyDef();
-        bd.set_type(b2_dynamicBody);
-        bd.set_position(new b2Vec2(6.5, 4.1));
-        lidBody = world.CreateBody(bd);
-        lidBody.CreateFixture(shape, 30.0);
-
-        anchor.Set(7.5, 4.0);
-        jd.Initialize(cradleBody, lidBody, anchor);
-        world.CreateJoint(jd);
-    }
-
-    //prop to support cradle
-    var propBody;
-    {
-        var shape = new b2PolygonShape();
-        shape.SetAsBox(0.1, 1.0);
-
-        var bd = new b2BodyDef();
-        bd.set_type(b2_dynamicBody);
-        bd.set_position(new b2Vec2(7.4, 1.0));
-
-        propBody = world.CreateBody(bd);
-        propBody.CreateFixture(shape, 10.0);
-    }
-
-    var djd = new b2DistanceJointDef();
-    djd.set_bodyA(seesawBody);
-    djd.set_bodyB(propBody);
-    djd.set_localAnchorA(new b2Vec2(6.0, 0.0));
-    djd.set_localAnchorB(new b2Vec2(0.0, -1.0));
-    var wpA = copyVec2(seesawBody.GetWorldPoint(djd.get_localAnchorA()));
-    var wpB = copyVec2(propBody.GetWorldPoint(djd.get_localAnchorB()));
-
-    //wpB.op_sub(wpA); wtf... why does op_add work fine but op_sub does nothing?
-    var d = new b2Vec2( wpB.get_x() - wpA.get_x(), wpB.get_y() - wpA.get_y() );
-
-    djd.set_length(d.Length());
-    world.CreateJoint(djd);
-
-    //balls
-    {
-        var radius = 0.2;
-
-        var shape = new b2CircleShape();
-        shape.set_m_radius(radius);
-
-        for (var i = 0; i < 4; ++i)
-        {
-            var bd = new b2BodyDef();
-            bd.set_type(b2_dynamicBody);
-            bd.set_position(new b2Vec2(5.9 + 2.0 * radius * i, 2.4));
-            var body = world.CreateBody(bd);
-            body.CreateFixture(shape, 10.0);
-        }
     }
 }
